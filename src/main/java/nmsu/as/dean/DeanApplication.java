780a363bf559b3811deaf8a0c76bd23aeb0be72a
bigname <tuanhiep.tran88@gmail.com>
2020-03-24 23:12:19 -0600

    Add SpringBootServletInitializer

--- src/main/java/nmsu/as/dean/DeanApplication.java
+++ src/main/java/nmsu/as/dean/DeanApplication.java
@@ -3,6 +3,8 @@ package nmsu.as.dean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,10 +12,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("nmsu/as/dean")
 @EnableAutoConfiguration
 @EnableJpaRepositories
-public class DeanApplication {
+public class DeanApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DeanApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeanApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeanApplication.class, args);
+    }
 
 }
